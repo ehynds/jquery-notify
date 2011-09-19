45bb45baa5ea139dbf88abd74d1092e5d82d7e1d
Scott Greenfield <sgreenfield@lyconic.com>
2011-09-19 13:52:37 -0500

    Remove notifications from DOM after closing.

--- src/jquery.notify.js
+++ src/jquery.notify.js
@@ -118,6 +118,7 @@ $.extend($.ech.notify.instance.prototype, {
 		this.element.fadeTo(speed, 0).slideUp(speed, function(){
 			self._trigger("close");
 			self.isOpen = false;
+            self.element.remove();
             $.ech.notify.openNotifications -= 1;
             self.parent.element.dequeue('notify');
 		});
