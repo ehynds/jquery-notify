76604175230b52d41b28ba8422e9ad19c10c5823
iliyi <iliyi.com@gmail.com>
2014-03-31 01:34:01 +0800

    accept template parameter pass on create options

--- src/jquery.notify.js
+++ src/jquery.notify.js
@@ -41,7 +41,8 @@
         template = null;
       }
 
-      var tpl = this.templates[ template || this.keys[0]];
+      // accept template parameter pass on create options
+      var tpl = opts.template || this.templates[ template || this.keys[0]];
 
       // remove default styling class if rolling w/ custom classes
       if(opts && opts.custom){
